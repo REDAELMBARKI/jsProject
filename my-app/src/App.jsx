faa99636ca9a0814654ea393d1269e5032e8b2ae
IREEL <redaelmbarki01@iCloud.com>
2025-03-16 00:00:22 +0000

    6

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,7 @@ import Prism from 'prismjs';
 import { Route,Routes } from 'react-router-dom'
 import './App.css'
 import ExercisePage from './pages/Exercise'
-import Home from "./pages/Home";
+import Home from "./pages/Home";sssss
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from './pages/Navbar';
