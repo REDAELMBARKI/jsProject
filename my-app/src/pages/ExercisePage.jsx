49001ec07950c7e784867ea0e14eb186c773866e
IREEL <redaelmbarki01@iCloud.com>
2025-03-18 23:23:27 +0000

    18/c5

--- my-app/src/pages/ExercisePage.jsx
+++ my-app/src/pages/ExercisePage.jsx
@@ -9,24 +9,27 @@ function ExercisePage() {
     const {page} = useParams()
     
     const {data} = useContext(FilteredData)
-    // const [dataForPage,setDataForPage] = useState(null)
-    // const [pageIndex,setPageIndex] = useState(1)
-  console.log(data)
+  
+  // console.log(data)
 
-  function Pagenate(page = 1, data) {
+  function Pagenate(page, data) {
     if (!data || data.length === 0) return null;
     return data[page - 1] || null;
 }
   
 
     const DATA_FOR_PAGE_INITIAL_STATE = {
-      dataForPage: Pagenate(1, data ?? []),
       pageIndex: 1,
+      dataForPage: Pagenate(1, data ?? []),
    };
 
    useEffect(() => {
+    
     if (data && data.length > 0) {
         dispatch({ type: 'update', payload: Pagenate(1, data) });
+    } 
+    else if(data.length === 0){
+      dispatch({ type: 'update'});
     }
 }, [data]);
 
@@ -34,7 +37,6 @@ function ExercisePage() {
     switch(action.type){
       case 'update': // New case to update data when it loads
       return {
-          ...state,
           dataForPage: action.payload,
           pageIndex: 1,
       }
@@ -73,9 +75,9 @@ function ExercisePage() {
          <SideMenu />
        </aside>
        <section  id='exercisesMain' className=' w-[80%] h-auto'>
-          <div id='exersices__Background' className=' border border-slate-200  '>
+          <div id='exersices__Background' className=' h-full border border-slate-200  '>
           
-           {    DataState.dataForPage &&
+           {    DataState.dataForPage ?
             <>
               <Exercises  key={DataState.dataForPage?.id}
                                          id={DataState.dataForPage?.id}
@@ -84,15 +86,20 @@ function ExercisePage() {
                                          solution={DataState.dataForPage?.solution} />
               <div className='flex justify-between mb-4'>
                     
-                      <button disabled={DataState.pageIndex === 1}  onClick={()=> dispatch({type :'prev'})}  className=" py-4 flex-col btn_prevNext w-[200px] gap-0  ">
+                      <button  disabled={DataState.pageIndex === 1}  onClick={()=> dispatch({type :'prev'})}  className=" py-4 flex-col btn_prevNext w-[200px] gap-0  ">
                         {/* <ChevronLeft /> */}
                         <p className=''>Exersice</p>
                       </button>
                       
-                      <button disabled={DataState.pageIndex === data.length} onClick={()=> dispatch({type :'next'})}  className="join-item btn_prevNext w-[200px] py-4 ">next</button>
+                      <button  disabled={DataState.pageIndex === data.length} onClick={()=> dispatch({type :'next'})}  className="join-item btn_prevNext w-[200px] py-4 ">next</button>
                   
               </div>
             </>
+
+            :
+            <div className='flex min-h-[100vh]'>
+               <span className='m-auto'>No exersices for this section</span>
+            </div>
            }
           
            
